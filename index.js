08af07de0239e0162d258e8b7475d87a80563a6e
VISHESH SHARMA <132488910+visheshsharma2003@users.noreply.github.com>
2024-09-19 12:41:01 +0530

    updation

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+let connection =require('./connection');
+const student=require('./routes/student');
+const app=express();
+
+connection();
+app.use(student);
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("serving is running on port 3000")
+    }
+    
+})
+//localhost:3000/add/student
